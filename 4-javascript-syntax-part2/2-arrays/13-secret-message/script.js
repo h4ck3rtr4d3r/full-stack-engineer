f742d2472a3465b4882c180b587765f28c5e6e40
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-10-27 06:15:46 -0400

    update

---
+++ 4-javascript-syntax-part2/2-arrays/13-secret-message/script.js
@@ -0,0 +1,40 @@
+let secretMessage = [
+  "Learning",
+  "is",
+  "not",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time",
+  "it",
+  "is",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out",
+  "-2015",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+];
+secretMessage.pop();
+console.log(secretMessage);
+console.log(secretMessage.length);
+secretMessage.push("to", "Program");
+console.log(secretMessage);
+secretMessage[7] = "right";
+console.log(secretMessage);
+secretMessage.shift();
+console.log(secretMessage);
+secretMessage.unshift("Learning");
+console.log(secretMessage);
+secretMessage.splice(6, 6, "know");
+console.log(secretMessage);
+console.log(secretMessage.join());
