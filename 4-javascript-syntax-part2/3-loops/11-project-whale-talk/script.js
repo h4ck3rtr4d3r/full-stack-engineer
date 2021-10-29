7dfd4ced450a3dd52ec5a589ea4f6957386df6d6
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-10-29 07:25:34 -0400

    update

---
+++ 4-javascript-syntax-part2/3-loops/11-project-whale-talk/script.js
@@ -0,0 +1,19 @@
+const input = "O Elefante é muito barulhento";
+const vowels = ["a", "e", "i", "o", "u"];
+let resultArray = [];
+
+// The loop iterate
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex++) {
+    if (input[inputIndex] === vowels[vowelsIndex]) {
+      if (input[inputIndex] === "e") {
+        resultArray.push("ee");
+      } else if (input[inputIndex] === "u") {
+        resultArray.push("uu");
+      } else {
+        resultArray.push(input[inputIndex]);
+      }
+    }
+  }
+}
+console.log(resultArray.join("").toUpperCase());
--- 4-javascript-syntax-part2/3-loops/3-the-for-loop/script.js
+++ 4-javascript-syntax-part2/3-loops/3-the-for-loop/script.js
@@ -2,7 +2,7 @@
 
 /**
  * A for loop contains three expressions separate by ; inside the parentheses:
- * 1. an initialization starts the loop and can also be used to dexlare the iterator variable
+ * 1. an initialization starts the loop and can also be used to declare the iterator variable
  * 2. a stopping condition is the condition tha the iterator variable is evaluated against - if the condition evaluates to true the code block will run, and if it evaluates to false the code will stop.
  * 3. an iteration statement is used to update the iterator variable on each loop
  */
