026d84baa1f1c81f6b1c0db5e158834be532df60
h4ck3rtr4d3r <86321646+h4ck3rtr4d3r@users.noreply.github.com>
2021-11-13 07:13:01 -0500

    update

--- 4-javascript-syntax-part2/5-iterators/3-functions-as-parameters/script.js
+++ 4-javascript-syntax-part2/5-iterators/3-functions-as-parameters/script.js
@@ -1,19 +1,14 @@
 /**
- * JavaScript functions behave like any other data type in the language; we can assign functions to variables, and we can reassign tem to new variables.
- * See below an annoyingly ling function:
- */
+Since functions can behave like any other type of data in JavaScript, it might not surprise you to learn that we can also pass functions (into other functions) as parameters. A higher-order function is a function that either accepts functions as parameters, returns a function, or both! We call the function that get passed in as parameters and invoked callback functions because they get called during the execution of the higher-order funtion. 
+When we pass a function in as an argument to another function, we don't invoke it. Invoking the function would evaluate to the return value of that function call. With callback, we pass in the function itself by typing the function name without the parentheses (that would evaluated to the result of calling the function):
+*/
 
-const announceThatIAmDoingImportantWork = () => {
-  console.log("I'm doing very important work!");
+const timeFuncRuntime = (funcParameter) => {
+  let t1 = Date.now();
+  funcParameter();
+  let t2 = Date.now;
+  return t2 - t1;
 };
 
-/**
- * What if we wanted to rename this function whitout sacrificing the source code? We can re-assign the function to a variable wih a suitably short name:
- */
-
-const busy = announceThatIAmDoingImportantWork;
-busy(); // This function call barely takes any space!
-
-/**
- *
- */
+const addOneToOne = () => 1 + 1;
+timeFuncRuntime(addOneToOne);
