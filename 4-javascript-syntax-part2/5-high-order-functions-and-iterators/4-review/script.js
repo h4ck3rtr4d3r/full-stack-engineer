6f56ed0f3bdd36d19915f5b024b4b6084a077dd4
h4ck3rtr4d3r <86321646+h4ck3rtr4d3r@users.noreply.github.com>
2021-11-14 05:32:51 -0500

    update

---
+++ 4-javascript-syntax-part2/5-high-order-functions-and-iterators/4-review/script.js
@@ -0,0 +1,10 @@
+/**
+By thinking about functions as data and learning about higher-order functions, you've taken important steps in being able to wrtie clean, modular code and take advantage of JavaScript's flexibility.
+Let's review  whatwe learned in this lesson:
+
+- Abstraction allows us to write complicated code in a way that's easy to reuse, debug and understand for human readers.
+- We can work with the same way we would any other type of day including reassingning them to new variables.
+- JavaScript function are first-class objects, so they have properties and methods like any object
+- Functions can be passed into other functions as parameters
+- A higher-order funtions is a function that eighter accepts functions as parameters, return a function, or both.
+*/
