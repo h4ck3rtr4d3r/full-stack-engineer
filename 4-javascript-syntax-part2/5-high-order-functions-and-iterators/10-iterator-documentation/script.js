9727cc574e058678b5605354261fdb4d5eacd308
h4ck3rtr4d3r <86321646+h4ck3rtr4d3r@users.noreply.github.com>
2021-11-20 05:55:22 -0500

    update

---
+++ 4-javascript-syntax-part2/5-high-order-functions-and-iterators/10-iterator-documentation/script.js
@@ -0,0 +1,27 @@
+/*
+The documentation for each methods contains several sections:
+1. A short definition
+2. A block with the correct syntax for using the method
+3. A list of parameters the method accepts or requires.
+4. The return value of the function.
+5. An extended description.
+6. Examples of the method's use.
+7. Other addiional information.
+
+*/
+
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some(() => {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+// console.log(interestingWords.every((word) => { } ));
